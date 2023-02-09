320410c11d37256c24478f02a333c2cc8b3aac36
jque4 <113262715+jque4@users.noreply.github.com>
2023-02-08 21:25:51 -0500

    Add files via upload

---
+++ notebooks/lab4.py
@@ -0,0 +1,25 @@
+import threading, time
+
+def function1(name):
+	for i in range(100000):
+		time.sleep(0.0001)
+	print(f'Function: "{name}" has finished excecuting. ')
+
+def function2(times):
+	currentTime = time.time()
+	elapsedTime = -1
+	for i in range(times):
+		function1(i)
+	elapsedTime = time.time() - currentTime
+	print(f'First function excecuted sequentially {times} times in {elapsedTime} seconds. ')
+
+def function3(times):
+	currentTime = time.time()
+	for i in range(times):
+		t = threading.Thread(target = function1, name = 'thread1', args = [i])
+		t.start()
+	elapsedTime = time.time() - currentTime
+	print(f'First function excecuted concurrently {times} times in {elapsedTime} seconds. ')
+
+function2(10)
+function3(10)
